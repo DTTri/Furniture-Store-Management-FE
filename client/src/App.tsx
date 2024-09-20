32b08080fc04e96cd442f6d4e928cc54b56c748d
Tri Duong <duongthuantri159@gmail.com>
2024-09-20 22:41:04 +0700

    Setup React and TailwindCSS

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-blue-950 text-white flex flex-col items-center justify-center">
+      <h1 className="text-3xl">Furniture Store Management</h1>
+    </div>
+  );
+}
+
+export default App;
