325d9aece64fc7b55bcf98b8f14fe346de534a64
Tri Duong <duongthuantri159@gmail.com>
2024-09-27 21:29:30 +0700

    Feat: Complete prototyping the Product Management Page

---
+++ client/src/components/productPage/ImportFromStockPopup.tsx
@@ -0,0 +1,108 @@
+import Product from "../../entities/Product";
+import { useState } from "react";
+export default function ImportFromStockPopup({
+  onClose,
+  products,
+}: {
+  onClose: () => void;
+  products: Product[];
+}) {
+  const [quantities, setQuantities] = useState(
+    products.reduce((acc, product) => {
+      acc[product.id] = 0;
+      return acc;
+    }, {} as Record<string, number>)
+  );
+
+  const handleQuantityChange = (productId: string, value: number) => {
+    setQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [productId]: Math.max(0, value),
+    }));
+  };
+
+  const handleAddToSell = () => {
+    products.forEach((product) => {
+      product.forSale += quantities[product.id];
+      product.stock -= quantities[product.id];
+    });
+    onClose();
+  };
+
+  return (
+    <div className="fixed inset-0 z-50 bg-black bg-opacity-50 flex items-center justify-center">
+      <div className="bg-white relative rounded-xl p-4 w-1/2 max-h-[80vh] overflow-hidden">
+        <button className="absolute top-2 right-2" onClick={onClose}>
+          x
+        </button>
+        <h2 className="text-xl font-bold">Nhập hàng từ kho</h2>
+        {/* Filter bar */}
+        <div className="flex items-center gap-2 mb-4">
+          <input
+            type="text"
+            placeholder="Tìm kiếm sản phẩm"
+            className="border border-gray-300 rounded-md p-1 w-full"
+          />
+          <button className="bg-blue-600 text-white px-2 py-1 rounded-md">
+            Tìm
+          </button>
+        </div>
+        <div className="table-container">
+          <table className="w-full">
+            <thead>
+              <tr>
+                <th>STT</th>
+                <th>Sản phẩm</th>
+                <th>Giá bán</th>
+                <th>Đang bán</th>
+                <th>Tồn kho</th>
+                <th>Tình trạng</th>
+                <th>Lấy ra</th>
+              </tr>
+            </thead>
+            <tbody className="max-h-96 overflow-y-auto">
+              {products.map((product, index) => (
+                <tr key={product.id}>
+                  <td className="text-center">{index + 1}</td>
+                  <td className="text-center">{product.name}</td>
+                  <td className="text-center">{product.productPrice}</td>
+                  <td className="text-center">{product.forSale}</td>
+                  <td className="text-center">{product.stock}</td>
+                  <td className="text-center">
+                    {product.isSelling ? "Sẵn sàng" : "Đang duyệt"}
+                  </td>
+                  <td className="text-center">
+                    <input
+                      type="number"
+                      className="border border-gray-300 rounded-md p-1 w-16"
+                      id="quantity"
+                      disabled={!product.isSelling}
+                      onChange={(e) => {
+                        e.target.value = Math.min(
+                          product.stock,
+                          parseInt(e.target.value)
+                        ).toString();
+                        handleQuantityChange(
+                          product.id,
+                          parseInt(e.target.value)
+                        );
+                      }}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className="flex justify-end mt-4">
+          <button
+            className="bg-blue-600 text-white px-4 py-2 rounded-md"
+            onClick={() => handleAddToSell()}
+          >
+            Thêm vào hóa đơn
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- client/src/pages/product/ProductPage.tsx
+++ client/src/pages/product/ProductPage.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import ProductCard from "../../components/productPage/ProductCard";
 import Product from "../../entities/Product";
 import NavBar from "../../components/NavBar";
+import ImportFromStockPopup from "../../components/productPage/ImportFromStockPopup";
 export default function ProductPage() {
   // hardcode data
   const initialProducts: Product[] = [
@@ -165,88 +166,123 @@ export default function ProductPage() {
     setInvoice([]);
   };
 
+  // open the import products from inventory modal
+  const [isImportFromStockPopupOpen, setIsImportFromStockPopupOpen] =
+    useState(false);
+
   return (
-    <div className="bg-gray-50 w-full h-screen max-h-screen flex gap-4 p-8">
+    <div className="bg-gray-100 w-full h-screen max-h-screen flex gap-4 p-8">
       <NavBar />
-      <div className="product-gallery w-3/5 h-full overflow-y-auto grid grid-cols-3 gap-4 p-8">
-        {products.map((product) => (
-          <ProductCard
-            key={product.id}
-            product={product}
-            onClick={() =>
-              invoice.find((item) => item.product.id === product.id)
-                ? handleRemoveFromInvoice(product)
-                : handleAddToInvoice(product)
-            }
-            isInInvoice={
-              !!invoice.find((item) => item.product.id === product.id)
-            }
-          />
-        ))}
-      </div>
-      <div className="invoice-container w-1/3 bg-white p-4 ">
-        <h3 className="text-2xl font-bold mb-4">Hóa đơn</h3>
-        <div className="invoice-table ">
-          <table className="min-w-full divide-y divide-gray-200">
-            <thead className="bg-gray-50">
-              <tr className="">
-                <th>STT</th>
-                <th>Sản phẩm</th>
-                <th>Giá</th>
-                <th>Số lượng</th>
-                <th>Thành tiền</th>
-              </tr>
-            </thead>
-            <tbody>
-              {invoice.map((item, index) => (
-                <tr key={item.product.id} className="min-h-8">
-                  <td className="text-center">{index + 1}</td>
-                  <td className="text-center">{item.product.name}</td>
-                  <td className="text-center">
-                    {item.product.productPrice.toLocaleString()}đ
-                  </td>
-                  <td className="text-center">
-                    <button
-                      onClick={() =>
-                        handleQuantityChange(item.product, item.quantity - 1)
-                      }
-                      disabled={item.quantity <= 1}
-                    >
-                      -
-                    </button>
-                    {item.quantity}
-                    <button
-                      onClick={() =>
-                        handleQuantityChange(item.product, item.quantity + 1)
-                      }
-                      disabled={item.quantity >= item.product.forSale}
-                    >
-                      +
-                    </button>
-                  </td>
-                  <td className="text-center">
-                    {(
-                      item.product.productPrice * item.quantity
-                    ).toLocaleString()}
-                    đ
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <div className="invoice-total mt-4 flex justify-between">
-            <p>Tổng tiền:</p>
-            <p className="total">{totalPrice.toLocaleString()}</p>
+      <div className="container">
+        <div className="header w-full bg-white flex gap-4 p-4">
+          <div className="search-bar w-2/5">
+            <input
+              type="text"
+              placeholder="Tìm sản phẩm"
+              className="w-full p-2 rounded-md border border-gray-500"
+            />
+          </div>
+          <button
+            onClick={() => setIsImportFromStockPopupOpen(true)}
+            className="bg-blue-600 text-white p-2 rounded-md"
+          >
+            Thêm hàng từ kho
+          </button>
+        </div>
+        <div className="body w-full flex p-4">
+          <div className="product-gallery w-3/5 h-full overflow-y-auto grid grid-cols-3 gap-4 p-8">
+            {products.map((product) => (
+              <ProductCard
+                key={product.id}
+                product={product}
+                onClick={() =>
+                  invoice.find((item) => item.product.id === product.id)
+                    ? handleRemoveFromInvoice(product)
+                    : handleAddToInvoice(product)
+                }
+                isInInvoice={
+                  !!invoice.find((item) => item.product.id === product.id)
+                }
+              />
+            ))}
+          </div>
+          <div className="invoice-container w-1/3 bg-white p-4 ">
+            <h3 className="text-2xl font-bold mb-4">Hóa đơn</h3>
+            <div className="invoice-table ">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr className="">
+                    <th>STT</th>
+                    <th>Sản phẩm</th>
+                    <th>Giá</th>
+                    <th>Số lượng</th>
+                    <th>Thành tiền</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {invoice.map((item, index) => (
+                    <tr key={item.product.id} className="min-h-8">
+                      <td className="text-center">{index + 1}</td>
+                      <td className="text-center">{item.product.name}</td>
+                      <td className="text-center">
+                        {item.product.productPrice.toLocaleString()}đ
+                      </td>
+                      <td className="text-center">
+                        <button
+                          onClick={() =>
+                            handleQuantityChange(
+                              item.product,
+                              item.quantity - 1
+                            )
+                          }
+                          disabled={item.quantity <= 1}
+                        >
+                          -
+                        </button>
+                        {item.quantity}
+                        <button
+                          onClick={() =>
+                            handleQuantityChange(
+                              item.product,
+                              item.quantity + 1
+                            )
+                          }
+                          disabled={item.quantity >= item.product.forSale}
+                        >
+                          +
+                        </button>
+                      </td>
+                      <td className="text-center">
+                        {(
+                          item.product.productPrice * item.quantity
+                        ).toLocaleString()}
+                        đ
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <div className="invoice-total mt-4 flex justify-between">
+                <p>Tổng tiền:</p>
+                <p className="total">{totalPrice.toLocaleString()}</p>
+              </div>
+            </div>
+
+            <button
+              onClick={() => handleCheckout()}
+              className="checkout-button bg-blue-600 text-white w-full py-2 rounded-md mt-4"
+            >
+              Thanh toán
+            </button>
           </div>
         </div>
-
-        <button
-          onClick={() => handleCheckout()}
-          className="checkout-button bg-blue-600 text-white w-full py-2 rounded-md mt-4"
-        >
-          Thanh toán
-        </button>
       </div>
+      {isImportFromStockPopupOpen && (
+        <ImportFromStockPopup
+          onClose={() => setIsImportFromStockPopupOpen(false)}
+          products={products}
+        />
+      )}
     </div>
   );
 }
