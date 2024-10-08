447acca80afe2fcbcf72e76bda1dcca72a9e7cea
Tri Duong <duongthuantri159@gmail.com>
2024-10-08 13:50:33 +0700

    Build: Install MUI component library

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // config colors
+        // primary: "#FF6363",
+        // secondary: "",
+      },
+    },
   },
   plugins: [],
 };
