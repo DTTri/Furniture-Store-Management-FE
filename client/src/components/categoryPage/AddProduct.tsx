84fa1d56f7caf49f842e34e17813f3bce96e51c9
Nguyen Trinh Vinh Thinh <vinhthinhvippro@gmail.com>
2024-11-10 21:03:16 +0700

    feat: implement CategoryDetail and AddProduct componet

---
+++ client/src/components/categoryPage/AddProduct.tsx
@@ -0,0 +1,189 @@
+import {
+    DataGrid,
+    GridActionsCellItem,
+    GridColDef,
+    GridRowParams,
+    GridToolbar,
+  } from "@mui/x-data-grid";
+  import { useEffect, useState } from "react";
+  import Category from "../../entities/Category";
+  import CloseIcon from '@mui/icons-material/Close';
+  import DeleteIcon from '@mui/icons-material/Delete';
+  import http from "../../api/http";
+  import { Product } from "../../entities";
+  import { Button } from "@mui/material";
+  
+  export default function AddProduct({
+    category,
+    onClose,
+  }: {
+    category: Category;
+    onClose: () => void;
+  }) {
+    const [productInCategory, setProductInCategory] = useState<Product[]>([]);
+  
+    useEffect(() => {
+      const fetchProductInCategory = async () => {
+        try {
+          const response = await http.get("/products/get-all-products");
+          if (response.data.EC === 0) {
+            const products: Product[] = response.data.DT;
+            const filterdProductInCategory = products.filter(
+              (product: Product) => product.catalogueId !== category.id
+            );
+            setProductInCategory(filterdProductInCategory);
+          } else {
+            console.log("Failed to fetch product in Category:", response.data.EM);
+          }
+        } catch (error) {
+          console.error("Error fetching products:", error);
+        }
+      };
+      fetchProductInCategory();
+    }, []);
+  
+    const columns: GridColDef[] = [
+      {
+        field: "index",
+        headerName: "STT",
+        flex: 0.5,
+        headerAlign: "center",
+        align: "center",
+      },
+      {
+        field: "id",
+        headerName: "ID",
+        flex: 0.5,
+        headerAlign: "center",
+        align: "center",
+      },
+      {
+        field: "name",
+        headerName: "Name",
+        flex: 1,
+        headerAlign: "center",
+        align: "center",
+      },
+      {
+        field: "category",
+        headerName: "Category",
+        flex: 1,
+        headerAlign: "center",
+        align: "center",
+      },
+      {
+        field: "price",
+        headerName: "Price",
+        flex: 1,
+        headerAlign: "center",
+        align: "center",
+      },
+      {
+        field: "status",
+        headerName: "Status",
+        flex: 1,
+        headerAlign: "center",
+        align: "center",
+      },
+      {
+        field: "warranty",
+        headerName: "Warranty",
+        flex: 1,
+        headerAlign: "center",
+        align: "center",
+      },
+      {
+        field: "available",
+        headerName: "Available",
+        flex: 1,
+        headerAlign: "center",
+        align: "center",
+      },
+      {
+        field: "quantity",
+        headerName: "Quantity",
+        flex: 1,
+        headerAlign: "center",
+        align: "center",
+      },
+      {
+        field: "actions",
+        type: "actions",
+        flex: 0.5,
+        getActions: (params: GridRowParams) => [
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={() => {
+              console.log("Delete product", params.row);
+            }}
+          />,
+        ],
+      }
+    ];
+    const rows = productInCategory.map((product, index) => ({
+      ...product,
+      index: index + 1,
+    }));
+    return (
+      <div className="fixed inset-0 z-50 bg-black bg-opacity-50 flex items-center justify-center">
+        <div className="relative popup bg-white flex flex-col justify-between flex-wrap gap-4 rounded-xl px-4 py-4 pt-11 w-2/3 min-w-[550px] h-[100vh] max-h-[65vh] overflow-auto">
+          <div className="header flex flex-row justify-between items-center bg-white mb-2">
+            <h2 className="text-2xl text-[#383E49] font-bold flex-1 uppercase">{category.name}</h2>
+            <CloseIcon sx={ { width: 27, height: 27 } } className="absolute top-2 right-[14px] hover:bg-slate-100 rounded-full cursor-pointer" onClick={onClose}/>
+            
+          </div>
+          <DataGrid
+            style={{
+              borderRadius: "10px",
+              backgroundColor: "white",
+              height: "100%",
+            }}
+            columns={columns}
+            rows={rows}
+            rowHeight={40}
+            initialState={{
+              pagination: {
+                paginationModel: {
+                  pageSize: 8,
+                },
+              },
+            }}
+            pageSizeOptions={
+              rows.length < 10 ? [10, rows.length] : [10, rows.length + 1]
+            }
+            slots={{ toolbar: GridToolbar }}
+            rowSelection={false}
+          />
+          <div className="buttons flex flex-row justify-end items-center gap-2">
+            <Button
+              variant="contained"
+              color="primary"
+              style={{
+                textTransform: "none",
+                fontSize: "14px"
+              }}
+              id="addProductButton"
+              onClick={() => {}}
+            >
+              Add
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              style={{
+                textTransform: "none",
+                fontSize: "14px",
+                backgroundColor: "#D91316",
+              }}
+              id=""
+              onClick={() => {}}
+            >
+              Cancle
+            </Button>
+            </div>
+        </div>
+      </div>
+    );
+  }
+  
--- client/src/pages/category/CategoryPage.tsx
+++ client/src/pages/category/CategoryPage.tsx
@@ -1,39 +1,41 @@
-import React, { useState } from 'react'
-import CategoryTable from '../../components/categoryPage/CategoryTable';
-import SearchIcon from '@mui/icons-material/Search';
-import { Button } from '@mui/material'
+import React, { useState } from "react";
+import CategoryTable from "../../components/categoryPage/CategoryTable";
+import SearchIcon from "@mui/icons-material/Search";
+import { Button } from "@mui/material";
+import DetailCategory from "../../components/categoryPage/CategoryDetail";
+import Category from "../../entities/Category";
 
 export default function CategoryPage() {
-  
   return (
-    <div className='bg-white w-full h-screen py-6 px-7'>
-        <div className="header buttons flex flex-row justify-between items-center bg-white mb-4">
-            <div className="search-bar w-[30%] px-1 mr-4 flex flex-row items-center border border-slate-400 rounded-xl overflow-hidden">
-                <input
-                    type="text"
-                    placeholder="Search Category"
-                    className="w-full py-2 px-[3px] rounded-md"
-                    style={{ border: '0', outline: 'none' }}
-                    onChange={(e) => {
-                    }}
-                    id="searchProductInput"
-                >
-                </input>
-                <SearchIcon className='hover:bg-slate-50 rounded-full p-1' sx={ { width: 35, height: 35} }/>
-            </div>
-            <Button
-            variant="contained"
-            color="primary"
-            style={{
-                textTransform: "none",
-            }}
-            id="addProductButton"
-            onClick={() => {}}
-            >
-            Add Category
-            </Button>
+    <div className="bg-white w-full h-screen py-6 px-7">
+      <div className="header buttons flex flex-row justify-between items-center bg-white mb-4">
+        <div className="search-bar w-[30%] px-1 mr-4 flex flex-row items-center border border-slate-400 rounded-xl overflow-hidden">
+          <input
+            type="text"
+            placeholder="Search Category"
+            className="w-full py-2 px-[3px] rounded-md"
+            style={{ border: "0", outline: "none" }}
+            onChange={(e) => {}}
+            id="searchProductInput"
+          ></input>
+          <SearchIcon
+            className="hover:bg-slate-50 rounded-full p-1"
+            sx={{ width: 35, height: 35 }}
+          />
         </div>
-        <CategoryTable/>
+        <Button
+          variant="contained"
+          color="primary"
+          style={{
+            textTransform: "none",
+          }}
+          id="addProductButton"
+          onClick={() => {}}
+        >
+          Add Category
+        </Button>
+      </div>
+      <CategoryTable />
     </div>
-  )
+  );
 }
